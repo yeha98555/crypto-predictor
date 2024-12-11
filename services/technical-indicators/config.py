047ec6be279d6c4e06c86c0ab66cb2af32e4add3
yeha98555 <yeha98555@gmail.com>
2024-12-11 20:58:56 +0800

    feat: Add candles management to limit max candles in state

--- services/technical-indicators/config.py
+++ services/technical-indicators/config.py
@@ -7,7 +7,7 @@ class Config(BaseSettings):
     kafka_input_topic: str
     kafka_output_topic: str
     kafka_consumer_group: str
-    num_candles_in_state: int = 60
+    max_candles_in_state: int = 60
 
 
 config = Config()
--- services/technical-indicators/run.py
+++ services/technical-indicators/run.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+from candle import update_candles
 from loguru import logger
 from quixstreams import Application
 
@@ -7,7 +10,7 @@ def main(
     kafka_input_topic: str,
     kafka_output_topic: str,
     kafka_consumer_group: str,
-    num_candles_in_state: int,
+    max_candles_in_state: int,
 ):
     """
     3 steps:
@@ -20,7 +23,7 @@ def main(
         kafka_input_topic (str): The topic to ingest candles from
         kafka_output_topic (str): The topic to send technical indicators to
         kafka_consumer_group (str): The consumer group to use
-        num_candles_in_state (int): The number of candles to keep in the state
+        max_candles_in_state (int): The maximum number of candles to keep in the state
 
     Returns:
         None
@@ -30,7 +33,7 @@ def main(
     logger.info(f'Kafka input topic: {kafka_input_topic}')
     logger.info(f'Kafka output topic: {kafka_output_topic}')
     logger.info(f'Kafka consumer group: {kafka_consumer_group}')
-    logger.info(f'Number of candles in state: {num_candles_in_state}')
+    logger.info(f'Number of candles in state: {max_candles_in_state}')
 
     app = Application(
         broker_address=kafka_broker_address,
@@ -47,7 +50,15 @@ def main(
         value_serializer='json',
     )
 
+    # Create a streaming dataframe from the input topic, so we can start transforming the data in real-time
     sdf = app.dataframe(topic=input_topic)
+
+    # Update the list of candles in the state
+    sdf = sdf.apply(
+        partial(update_candles, max_candles_in_state=max_candles_in_state),
+        stateful=True,
+    )
+
     sdf = sdf.update(lambda value: logger.info(f'candle: {value}'))
     sdf = sdf.to_topic(output_topic)
 
@@ -62,5 +73,5 @@ if __name__ == '__main__':
         kafka_input_topic=config.kafka_input_topic,
         kafka_output_topic=config.kafka_output_topic,
         kafka_consumer_group=config.kafka_consumer_group,
-        num_candles_in_state=config.num_candles_in_state,
+        max_candles_in_state=config.max_candles_in_state,
     )
