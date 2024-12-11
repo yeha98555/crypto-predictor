4aa89698eebd7b6cbfd863a45cee59797d9acb43
yeha98555 <yeha98555@gmail.com>
2024-12-12 02:27:28 +0800

    chore: Add setup for the to-feature-store microservice

---
+++ services/to-feature-store/README.md
@@ -0,0 +1,3 @@
+## To Feature Store
+
+This services read messages from the `technical-indicators` service and writes them to the feature store.
---
+++ services/to-feature-store/run.py
@@ -0,0 +1,9 @@
+from loguru import logger
+
+
+def main():
+    logger.info('Hello from to-feature-store!')
+
+
+if __name__ == '__main__':
+    main()
